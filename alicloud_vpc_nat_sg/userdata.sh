b3a6e09df21bacddfc2b1bccd3e30ca15b251bc7
dhutsj <jszxtsj@163.com>
2022-07-23 14:12:17 +0800

    update userdata

--- alicloud_vpc_nat_sg/userdata.sh
+++ alicloud_vpc_nat_sg/userdata.sh
@@ -5,7 +5,7 @@ yum install nginx -y
 systemctl enable nginx
 systemctl start nginx
 # echo "Hello World. The time is now $(date -R)!" | tee /root/userdata_test.txt
-sed -i "s/Welcome to/Hello123/g" /usr/share/nginx/html/index.html
+sed -i "s/Welcome to/Happy Chinese Valentine's Day/g" /usr/share/nginx/html/index.html
 # systemctl restart nginx
 
 
