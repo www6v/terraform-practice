4abd4e6562a41a45dffec5d52cceb559c7377a22
dhutsj <shuangjie.tian@elsevier.com>
2022-07-26 17:22:55 +0800

    tweak code

--- invoke_module/main.tf
+++ invoke_module/main.tf
@@ -1,5 +1,6 @@
 module "tsj_demo" {
-  source = "git::https://jihulab.com/dhutsj/terraform-practice.git//alicloud_vpc_nat_sg?ref=main"
+  # source = "git::https://jihulab.com/dhutsj/terraform-practice.git//alicloud_vpc_nat_sg?ref=main"
+  source = "../alicloud_vpc_nat_sg"
 
   vpc_name = var.vpc_name
 }
